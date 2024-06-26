64a297bb54d9570a17198a78cca851d95203fa07
technuma <technuma@gmail.com>
2024-06-26 11:58:04 +0900

    add initializer sample

--- lib/technuma_hello_world.rb
+++ lib/technuma_hello_world.rb
@@ -2,9 +2,29 @@
 
 require_relative "technuma_hello_world/version"
 
+# module TechnumaHelloWorld
+#   class Error < StandardError; end
+#   def self.hello
+#     "Hello World"
+#   end
+# end
 module TechnumaHelloWorld
-  class Error < StandardError; end
-  def self.hello
-    "Hello World"
+  class Railtie < Rails::Railtie
+    initializer 'predicate_builder_extension.configure_rails_initialization' do
+      ActiveSupport.on_load(:active_record) do
+        ActiveRecord::PredicateBuilder.prepend Module.new {
+          def [](attr_name, value, operator = nil)
+            if !operator && attr_name.end_with?(">", ">=", "<", "<=")
+              /\A(?<attr_name>.+?)\s*(?<operator>>|>=|<|<=)\z/ =~ attr_name
+              operator = OPERATORS[operator]
+            end
+
+            super
+          end
+
+          OPERATORS = { ">" => :gt, ">=" => :gteq, "<" => :lt, "<=" => :lteq }.freeze
+        }
+      end
+    end
   end
 end
