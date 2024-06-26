be60a7c8d7e6a0032b828138de973639842daef9
technuma <technuma@gmail.com>
2024-06-26 18:41:32 +0900

    add dependency to gemspec

--- lib/technuma_hello_world.rb
+++ lib/technuma_hello_world.rb
@@ -2,29 +2,27 @@
 
 require_relative "technuma_hello_world/version"
 
-# module TechnumaHelloWorld
-#   class Error < StandardError; end
-#   def self.hello
-#     "Hello World"
-#   end
-# end
+require "technuma_hello_world/version"
+require "active_support"
+require "active_record"
+
 module TechnumaHelloWorld
-  class Railtie < Rails::Railtie
-    initializer 'predicate_builder_extension.configure_rails_initialization' do
-      ActiveSupport.on_load(:active_record) do
-        ActiveRecord::PredicateBuilder.prepend Module.new {
-          def [](attr_name, value, operator = nil)
-            if !operator && attr_name.end_with?(">", ">=", "<", "<=")
-              /\A(?<attr_name>.+?)\s*(?<operator>>|>=|<|<=)\z/ =~ attr_name
-              operator = OPERATORS[operator]
-            end
+  class Error < StandardError; end
 
-            super
-          end
-
-          OPERATORS = { ">" => :gt, ">=" => :gteq, "<" => :lt, "<=" => :lteq }.freeze
-        }
+  module PredicateBuilderExtension
+    def [](attr_name, value, operator = nil)
+      if !operator && attr_name.end_with?(">", ">=", "<", "<=")
+        /\A(?<attr_name>.+?)\s*(?<operator>>|>=|<|<=)\z/ =~ attr_name
+        operator = OPERATORS[operator]
       end
+
+      super
     end
+
+    OPERATORS = { ">" => :gt, ">=" => :gteq, "<" => :lt, "<=" => :lteq }.freeze
   end
+end
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::PredicateBuilder.prepend TechnumaHelloWorld::PredicateBuilderExtension
 end
