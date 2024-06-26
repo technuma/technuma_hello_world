96687b40882c03b6b86e1b55be7c145b18c1518d
technuma <technuma@gmail.com>
2024-06-26 11:21:19 +0900

    first commit

---
+++ lib/technuma_hello_world.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "technuma_hello_world/version"
+
+module TechnumaHelloWorld
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/technuma_hello_world/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TechnumaHelloWorld
+  VERSION = "0.1.0"
+end
