1d2cd9b73dbea657773d8a7fd3948dfd4ae9bc40
technuma <technuma@gmail.com>
2024-07-01 11:42:45 +0900

    add 'requrie active_record'

--- lib/technuma_hello_world.rb
+++ lib/technuma_hello_world.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "technuma_hello_world/version"
+require "active_record"
 
 module TechnumaHelloWorld
   class Error < StandardError; end
+
   def self.hello
     "Hello World"
   end
