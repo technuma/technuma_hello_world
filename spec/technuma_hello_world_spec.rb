dfdb6b5576506c4dfba98f96923225e844b5c54e
technuma <technuma@gmail.com>
2024-07-03 11:15:47 +0900

    format and add code comments

--- spec/technuma_hello_world_spec.rb
+++ spec/technuma_hello_world_spec.rb
@@ -22,11 +22,15 @@ RSpec.describe TechnumaHelloWorld do
       belongs_to :post
     end
   end
+
   it "has a version number" do
     expect(TechnumaHelloWorld::VERSION).not_to be nil
   end
+
+  # https://github.com/rails/rails/pull/39863/files#diff-1721af6d2f906e7e5eec9cac9a7707cd051c1a1f5ffb1b38c69a6c5a52ae4e9aR318-R326
   describe "exists with large number" do
     let!(:post) { Post.create! }
+
     it "checks post existence with large number conditions" do
       Post.create!
       expect(Post.where("id >": -9_223_372_036_854_775_809).exists?).to be true
@@ -40,9 +44,12 @@ RSpec.describe TechnumaHelloWorld do
       expect(Post.where("id <=": -9_223_372_036_854_775_809).exists?).to be false
     end
   end
+
+  # https://github.com/rails/rails/pull/39863/files#diff-fba6d35ef65b69650470b26fbf8e945446b6bb92c543e944015908a9fe200d62R101-R106
   describe "datetime precision" do
     let(:date) { ::Time.utc(2014, 8, 17, 12, 30, 0, 999_999) }
     let!(:post) { Post.create!(created_at: date, updated_at: date) }
+
     it "formats datetime according to precision" do
       expect(Post.find_by("created_at >= ?", date)).to be_nil
       expect(Post.where("updated_at >= ?", date).count).to eq(0)
