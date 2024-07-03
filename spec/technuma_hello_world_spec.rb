9fe9d280eba90b4c3d6f589c2d54c85e59378fbe
technuma <technuma@gmail.com>
2024-07-03 11:04:16 +0900

    moved database setup to a before block

--- spec/technuma_hello_world_spec.rb
+++ spec/technuma_hello_world_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe TechnumaHelloWorld do
-  it "has a version number" do
-    expect(TechnumaHelloWorld::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
+  before do
     ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
     ActiveRecord::Base.logger = Logger.new(STDOUT)
     ActiveRecord::Schema.define do
@@ -25,12 +21,18 @@ RSpec.describe TechnumaHelloWorld do
     class Comment < ActiveRecord::Base
       belongs_to :post
     end
-
-    date = ::Time.utc(2014, 8, 17, 12, 30, 0, 999_999)
-    Post.create!(created_at: date, updated_at: date)
-    expect(Post.find_by("created_at >= ?", date)).to be_nil
-    expect(Post.where("updated_at >= ?", date).count).to eq(0)
-    expect(Post.find_by("created_at >=": date)).to be_truthy
-    expect(Post.where("updated_at >=": date).count).to eq(1)
+  end
+  it "has a version number" do
+    expect(TechnumaHelloWorld::VERSION).not_to be nil
+  end
+  describe "datetime precision" do
+    let(:date) { ::Time.utc(2014, 8, 17, 12, 30, 0, 999_999) }
+    let!(:post) { Post.create!(created_at: date, updated_at: date) }
+    it "formats datetime according to precision" do
+      expect(Post.find_by("created_at >= ?", date)).to be_nil
+      expect(Post.where("updated_at >= ?", date).count).to eq(0)
+      expect(Post.find_by("created_at >=": date)).to be_truthy
+      expect(Post.where("updated_at >=": date).count).to eq(1)
+    end
   end
 end
