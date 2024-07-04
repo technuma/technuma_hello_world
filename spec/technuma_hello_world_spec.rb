05e4b7c058bba6200c5beac7dcb9c843a049cb38
technuma <technuma@gmail.com>
2024-07-04 17:05:15 +0900

    remove tests for large number existence

--- spec/technuma_hello_world_spec.rb
+++ spec/technuma_hello_world_spec.rb
@@ -28,24 +28,6 @@ RSpec.describe TechnumaHelloWorld do
       end
     end
 
-    # https://github.com/rails/rails/pull/39863/files#diff-1721af6d2f906e7e5eec9cac9a7707cd051c1a1f5ffb1b38c69a6c5a52ae4e9aR318-R326
-    describe "exists with large number" do
-      let!(:post) { Post.create! }
-
-      it "checks post existence with large number conditions" do
-        Post.create!
-        expect(Post.where("id >": -9_223_372_036_854_775_809).exists?).to be true
-        expect(Post.where("id >=": -9_223_372_036_854_775_809).exists?).to be true
-        expect(Post.where("id <": 9_223_372_036_854_775_808).exists?).to be true
-        expect(Post.where("id <=": 9_223_372_036_854_775_808).exists?).to be true
-
-        expect(Post.where("id >": 9_223_372_036_854_775_808).exists?).to be false
-        expect(Post.where("id >=": 9_223_372_036_854_775_808).exists?).to be false
-        expect(Post.where("id <": -9_223_372_036_854_775_809).exists?).to be false
-        expect(Post.where("id <=": -9_223_372_036_854_775_809).exists?).to be false
-      end
-    end
-
     # https://github.com/rails/rails/pull/39863/files#diff-fba6d35ef65b69650470b26fbf8e945446b6bb92c543e944015908a9fe200d62R101-R106
     describe "datetime precision" do
       let(:date) { ::Time.utc(2014, 8, 17, 12, 30, 0, 999_999) }
